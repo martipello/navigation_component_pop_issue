b8869dad4a960152ab4aceb26fd4c41006ce793f
martin seal <martin.seal@spicatech.co.uk>
2019-05-18 08:48:51 +0100

    issue commit

---
+++ app/src/main/java/com/sealstudios/nav_issue/BlankFragment.java
@@ -0,0 +1,33 @@
+package com.sealstudios.nav_issue;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
+
+public class BlankFragment extends Fragment {
+
+
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+}
